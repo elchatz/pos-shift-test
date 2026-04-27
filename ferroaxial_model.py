c51932e3f5e9da9a87e7d685ffce4bcb6a050eca
Stepan Tsirkin <stepan.tsirkin@epfl.ch>
2026-04-27 19:05:08 +0200

    spinful model

--- ferroaxial_model.py
+++ ferroaxial_model.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pythtb
 
-def model_ferroaxial_pythtb(mu=0.0, t=1.0, tp=1.0, Delta=1.0, t2=0.5, t3=1.0, a=1.0, c=1.0, rot_deg=0):
+def model_ferroaxial_pythtb(mu=0.0, t=1.0, tp=1.0, Delta=1.0, t2=0.5, t3=1.0, a=1.0, c=1.0, 
+                            spin=False, t_spin=0.0,
+                            rot_deg=0):
 
     rot_alpha = rot_deg * np.pi/180  # Rotation angle in radians
     rot_mat = np.array([[np.cos(rot_alpha), -np.sin(rot_alpha), 0],
@@ -17,7 +19,7 @@ def model_ferroaxial_pythtb(mu=0.0, t=1.0, tp=1.0, Delta=1.0, t2=0.5, t3=1.0, a=
     #lattice = pythtb.Lattice(lat_vecs=lat, orb_vecs=orb, periodic_dirs=[0, 1, 2])
     #my_model = pythtb.TBModel(lattice)
 
-    my_model = pythtb.tb_model(3, 3, lat, orb)
+    my_model = pythtb.tb_model(3, 3, lat, orb, nspin=2 if spin else 1)
     my_model.set_onsite([Delta - mu, -Delta - mu])
 
     
@@ -40,11 +42,18 @@ def model_ferroaxial_pythtb(mu=0.0, t=1.0, tp=1.0, Delta=1.0, t2=0.5, t3=1.0, a=
         my_model.set_hop(-t3, 0, 1, [ 0, -2,  0])
 
 
-    # The tc term (Diagonal Hoppings)
-    if t2 != 0:
-        for shift in [[0,1,0], [1,0,0], [1,1,0]]: # Skipping [0,0,0] for diagonal
-            for i in range(2):
-                my_model.set_hop(t2, i, i, shift)
+    if spin:
+        sigma_0 = np.array([1., 0., 0., 0])
+        sigma_z = np.array([0., 0., 0., 1])
+    else:
+        sigma_0 = 1
+        sigma_z = 0
+
+    for shift in [[0,1,0], [1,0,0], [-1,-1,0]]: # Skipping [0,0,0] for diagonal
+        for i, sign in enumerate([1, -1]):
+            # In-plane second neighbor hoppings (t2)
+            hop = t2*sigma_0 + 1j*t_spin*sign*sigma_z
+            my_model.set_hop(hop, i, i, shift)
 
     return my_model
 
